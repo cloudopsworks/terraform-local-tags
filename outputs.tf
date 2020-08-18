2ea1c30d83ffa5391229e8b3dddf4994d1eae658
berahac <cristian.beraha@e.lastic.guru>
2020-08-18 15:21:47 -0300

    added lower-case outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,15 @@
 output "locals" {
   description = "All local scoped varialbes set"
   value = {
-    name_tag           = local.name_tag
-    object_name_tag    = local.object_name_tag
-    organization_name  = local.organization_name
-    environment_name   = local.environment_name
-    common_tags        = local.common_tags
-    common_tags_string = local.common_tags_string
+    name_tag             = local.name_tag
+    name_tag_lc          = lower(local.name_tag)
+    object_name_tag      = local.object_name_tag
+    object_name_tagi_lc  = lower(local.object_name_tag)
+    organization_name    = local.organization_name
+    organization_name_lc = lower(local.organization_name)
+    environment_name     = local.environment_name
+    environment_name_lc  = lower(local.environment_name)
+    common_tags          = local.common_tags
+    common_tags_string   = local.common_tags_string
   }
 }
