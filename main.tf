13048ecc7641410a5bdce89703a5b1afd6fcca1f
berahac <cristian.beraha@e.lastic.guru>
2020-08-17 21:39:26 -0300

    changed to object type

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 locals {
-  name_tag        = join(" ", compact([var.organization_name, var.org_unit_name, var.environment_name, var.environment_type]))
-  object_name_tag = join("", compact([var.organization_name, var.org_unit_name, var.environment_name, var.environment_type]))
+  name_tag        = join(" ", compact([var.env.organization_name, var.env.org_unit_name, var.env.environment_name, var.env.environment_type]))
+  object_name_tag = join("", compact([var.env.organization_name, var.env.org_unit_name, var.env.environment_name, var.env.environment_type]))
 
-  organization_name = join("-", compact([var.organization_name, var.org_unit_name]))
-  environment_name  = join("-", compact([var.organization_name, var.org_unit_name, var.environment_name, var.environment_type]))
+  organization_name = join("-", compact([var.env.organization_name, var.env.org_unit_name]))
+  environment_name  = join("-", compact([var.env.organization_name, var.env.org_unit_name, var.env.environment_name, var.env.environment_type]))
 
   common_tags = {
-    organization-full-name = join(" ", compact([var.organization_name, var.org_unit_name, var.environment_name, var.environment_type]))
-    organization           = var.organization_name
-    organization-unit      = var.org_unit_name
-    application-name       = var.environment_name
-    application-type       = var.environment_type
+    organization-full-name = join(" ", compact([var.env.organization_name, var.env.org_unit_name, var.env.environment_name, var.env.environment_type]))
+    organization           = var.env.organization_name
+    organization-unit      = var.env.org_unit_name
+    application-name       = var.env.environment_name
+    application-type       = var.env.environment_type
   }
 
   common_tags_string = join(",", [for k, v in local.common_tags : format("%s=%s", k, v)])
--- variables.tf
+++ variables.tf
@@ -1,18 +1,15 @@
-variable "organization_name" {
-  description = "Organization Name to use in labeling"
-  default     = ""
-}
-variable "org_unit_name" {
-  description = "Organization Unit Name to use with labeling"
-  default     = ""
-}
-
-variable "environment_name" {
-  description = "Environment name for use with labeling"
-  default     = ""
-}
-
-variable "environment_type" {
-  description = "Environment type for use with labeling"
-  default     = ""
+variable "env" {
+  description = "Environment definition object"
+  type = object({
+    organization_name = string
+    org_unit_name     = string
+    environment_name  = string
+    environment_type  = string
+  })
+  default = {
+    organization_name = ""
+    org_unit_name     = ""
+    environment_name  = ""
+    environment_type  = ""
+  }
 }
